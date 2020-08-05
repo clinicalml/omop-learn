1ca3c44492d42cad95e6eda9a434e92f5d7fff42
justinlimkz <justinlimkz@gmail.com>
2020-08-04 22:22:22 -0400

    Flexible feature SQL scripts

---
+++ sql/Features/conditions_relative.sql
@@ -0,0 +1,18 @@
+select 
+    a.person_id,
+    a.condition_concept_id || ' - condition - ' || coalesce (
+        c.concept_name, 'no match'
+    ) as concept_name,
+    date '2100-01-01' - b.end_date + a.condition_start_datetime as feature_start_date,      -- dummy date to right-align features
+    b.start_date as person_start_date,
+    b.end_date as person_end_date
+from 
+    {cdm_schema}.condition_occurrence a
+inner join
+    {cohort_table} b
+on 
+    a.person_id = b.person_id
+left join
+    {cdm_schema}.concept c
+on 
+    c.concept_id = a.condition_concept_id
---
+++ sql/Features/procedures_relative.sql
@@ -0,0 +1,20 @@
+select 
+    a.person_id,
+    a.procedure_concept_id || ' - procedure - ' || coalesce (
+        c.concept_name, 'no match'
+    ) as concept_name,
+    date '2100-01-01' - b.end_date + a.procedure_datetime as feature_start_date,          -- dummy date to right-align features
+    b.start_date as person_start_date,
+    b.end_date as person_end_date
+from 
+    {cdm_schema}.procedure_occurrence a
+inner join
+    {cohort_table} b
+on 
+    a.person_id = b.person_id
+left join
+    {cdm_schema}.concept c
+on 
+    c.concept_id = a.procedure_concept_id
+
+
